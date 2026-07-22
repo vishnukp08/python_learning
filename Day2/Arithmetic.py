9a367ca38e3bbc5756388d704d10390f74919a38
vishnukp08 <kpvishnu814@gmail.com>
2026-07-22 20:29:10 +0530

    Add Day2

---
+++ Day2/Arithmetic.py
@@ -0,0 +1,10 @@
+a = 10
+b = 4
+
+print(a + b)  # addition
+print(a - b)  # subtraction
+print(a * b)  # multiplication
+print(a / b)  # division
+print(a // b) # floor division
+print(a % b)  # remainder
+print(a ** b) # exponent
