01297d783b664f8927db5767262d6a4d9423af04
Vishnu KP <kpvishnu814@gmail.com>
2026-09-18 00:26:46 +0530

    done

---
+++ Day2/Arithmetic_Calc.py
@@ -0,0 +1,4 @@
+n1=input("Enter first number: ")
+n2=input("Enter second number: ")
+
+print("Add:",int(n1)+int(n2),"\nSub:",int(n1)-int(n2),"\nMul:",int(n1)*int(n2),"\nDiv:",int(n1)/int(n2),"\nMod:",int(n1)%int(n2),"\nFloor Div:",int(n1)//int(n2),"\nPower:",int(n1)**int(n2))
