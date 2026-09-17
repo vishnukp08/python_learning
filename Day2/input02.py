6f9dbdb37abfa54c9beadaad5079562218dc8ee8
Vishnu KP <kpvishnu814@gmail.com>
2026-09-18 00:20:15 +0530

    added ip02

---
+++ Day2/input02.py
@@ -0,0 +1,7 @@
+#age=input("Enter your age: ")
+#print("Age*2 is",age*2)
+#print(age*2)
+#This is wrong because age is a string and we cannot multiply a string with an integer. We need to convert the string to an integer first. it will display the number twice.
+
+age=int(input("Enter age: "))
+print("Age*2 is:",age*2)
