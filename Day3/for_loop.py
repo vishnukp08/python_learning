125245e8687e6450fac744f72ccbcf076365a871
Vishnu KP <kpvishnu814@gmail.com>
2026-09-18 23:35:21 +0530

    done

---
+++ Day3/for_loop.py
@@ -0,0 +1,22 @@
+#for i in range(1,10,2):
+#    print(i)
+
+#for letter in "Vishnu":
+#    print(letter)
+'''
+for i in range(1,10):
+    if i==5:
+        break
+    print(i)
+'''
+
+'''
+for i in range(1,10):
+    if i%2==1:
+        continue
+    print(i)
+'''
+
+for i in range(3):
+    for j in range(2):
+        print(i,j)
