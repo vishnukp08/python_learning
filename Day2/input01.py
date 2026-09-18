10ee62efcc59546c52138f1c916de5ad53458da9
Vishnu KP <kpvishnu814@gmail.com>
2026-09-18 10:34:31 +0530

    modified ip01

--- Day2/input01.py
+++ Day2/input01.py
@@ -1,2 +1,10 @@
+'''
 name=input("Enter your name: ")
 print("Your name is",name)
+'''
+
+#name=input("Enter name: ")
+#print("Your name is",name,"!")
+
+name=input("Enter name: ")
+print("You are, "+name+"!")
