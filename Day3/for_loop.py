4bb85a610a68265b392c34826960d4c5a0687e21
Vishnu KP <kpvishnu814@gmail.com>
2026-09-18 23:43:55 +0530

    modified

--- Day3/for_loop.py
+++ Day3/for_loop.py
@@ -16,7 +16,12 @@ for i in range(1,10):
         continue
     print(i)
 '''
-
+'''
 for i in range(3):
     for j in range(2):
         print(i,j)
+'''
+
+for i in range(1,20):
+    if i%2!=0:
+        print(i)
