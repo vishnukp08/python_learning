18703483ffa1b6524876cd72ce123588e0eb9eb8
Vishnu KP <kpvishnu814@gmail.com>
2026-09-23 18:45:38 +0530

    Modified

--- Day1/Function.py
+++ Day1/Function.py
@@ -2,8 +2,8 @@ calc_to_unit=24
 units="hours"
 
 def day_to_units(n):
-	print(f"{n} days are {n * calc_to_unit} {units}")
-
-day_to_units(10)
-day_to_units(20)
-day_to_units(100)
+	return f"{n} days are {n * calc_to_unit} {units}"
+user=input("Enter no.of days: ")
+user=int(user)
+res=day_to_units(user)
+print(res)
