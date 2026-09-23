0d480169790fd53141662d3bac2086142085b114
Vishnu KP <kpvishnu814@gmail.com>
2026-09-23 19:11:40 +0530

    Modified

--- Day1/Function.py
+++ Day1/Function.py
@@ -2,7 +2,12 @@ calc_to_unit=24
 units="hours"
 
 def day_to_units(n):
-	return f"{n} days are {n * calc_to_unit} {units}"
+	if n>0:
+		return f"{n} days are {n * calc_to_unit} {units}"
+	elif n==0:
+		return "You entered 0"
+	else:
+		return "You entered a -ve number"
 user=input("Enter no.of days: ")
 user=int(user)
 res=day_to_units(user)
