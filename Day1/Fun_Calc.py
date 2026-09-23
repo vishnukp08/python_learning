be300bb8353acadfe0d4fd23ed396e5b9948b466
Vishnu KP <kpvishnu814@gmail.com>
2026-09-23 23:11:58 +0530

    done

---
+++ Day1/Fun_Calc.py
@@ -0,0 +1,8 @@
+a=int(input("Enter number: "))
+b=int(input("Enter number: "))
+
+
+def calc(a,b):
+    print(f"Add = {a+b} \nSub = {a-b}")
+    
+calc(a,b)
---
+++ Day4/List.py
@@ -0,0 +1,2 @@
+items=["apple","banana","cherry"]
+print(items)
