0fc653717b0150dde59414f9a8b2fb11d944c82c
Vishnu KP <kpvishnu814@gmail.com>
2026-09-24 22:07:55 +0530

    modified

--- Day1/Function.py
+++ Day1/Function.py
@@ -1,27 +1,35 @@
 cal_units = 24 * 60 * 60
 units = "Seconds"
 
+
 def calc(n):
     return f"{n} days are {n * cal_units} {units}"
-        
-        
+
+
 def validate():
     try:
-        user_in=int(num)
+        user_in = int(n)
         if user_in > 0:
-            res=calc(user_in)
+            res = calc(user_in)
             print(res)
-        elif user_in==0:
+        elif user_in == 0:
             print("You entered 0")
         else:
             print("You entered negative number")
     except:
         print("Enter valid number")
-        
-user=""
+
+
+user = ""
 
 while user.lower() != "exit":
-	user=input("Enter no.of days: ")
-	print(user.split())
-	for num in user.split():
-		validate()
+    user = input("Enter no.of days: ")
+
+    # using Split DataType to add items to a list
+    print(user.split())
+    
+    # using SET DataType to avoid duplicate number from the list
+    print(set(user.split()))
+    
+    for n in set(user.split()):
+        validate()
