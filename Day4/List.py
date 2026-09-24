86af8ffdd6367d4d3dbc84043bf0314207103551
Vishnu KP <kpvishnu814@gmail.com>
2026-09-24 21:57:43 +0530

    modified

--- Day4/List.py
+++ Day4/List.py
@@ -1,2 +1,10 @@
-items=["apple","banana","cherry"]
+items = ["apple", "banana", "cherry"]
+items.append("orange")
+print(items)
+print(items[2])
+
+items.insert(0, "mango")
+print(items)
+
+items.remove("banana")
 print(items)
